3c7d77db90d32d71ad7446d45ab9975be3ee1391
Sam Cogan <mail@samcogan.com>
2023-01-14 16:55:16 +0000

    Episode 3

--- __main__.py
+++ __main__.py
@@ -5,21 +5,19 @@ from pulumi_azure_native import storage
 from pulumi_azure_native import resources
 from datetime import date
 
-tags = {
-    "OwnedBy" : "Sam Cogan",
-    "DateCreated" : date.today().isoformat()
- }
+config = pulumi.Config()
 
-createStorageContainer=False
+tags = config.require_object("tags")
+tags["dateCreated"] = date.today().isoformat()
 
 rg = resources.ResourceGroup('resourceGroup',
- resource_group_name= "PulumiForItPros",
- location= "WestEurope",
+ resource_group_name= config.require("resourceGroupName"),
+ location= config.require("location"),
  tags = tags
 ) 
 
 stg = storage.StorageAccount('stg',
-    account_name= "itprosstg01",
+    account_name= config.require("namePrefix")+"stg01",
     resource_group_name= rg.name,
     location= rg.location,
     sku= storage.SkuArgs(
@@ -30,7 +28,7 @@ stg = storage.StorageAccount('stg',
 
 )
 
-if createStorageContainer:
+if config.require_bool("createStorageContainer"):
     container = storage.BlobContainer("container",
     account_name= stg.name,
     container_name= "container1",
